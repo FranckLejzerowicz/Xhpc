5da94e9eed8fbd455be22634b777d9768d432891
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2022-05-03 09:50:15 +0200

    remove unused function and update tests

--- Xhpc/test/test_nodes.py
+++ Xhpc/test/test_nodes.py
@@ -57,16 +57,16 @@ class TestGetNodelist(unittest.TestCase):
         self.assertEqual(exp, obs)
 
 
-class TestAllocateNodes(unittest.TestCase):
-
-    def setUp(self) -> None:
-        pass
-
-    def test_allocate_nodes(self):
-        allocate_nodes()
-
-    def tearDown(self) -> None:
-        pass
+# class TestAllocateNodes(unittest.TestCase):
+#
+#     def setUp(self) -> None:
+#         pass
+#
+#     def test_allocate_nodes(self):
+#         allocate_nodes()
+#
+#     def tearDown(self) -> None:
+#         pass
 
 
 if __name__ == '__main__':
