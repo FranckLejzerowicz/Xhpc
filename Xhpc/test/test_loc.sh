78219b00c8b2b44339817bca13563f447b98cddb
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2022-05-06 09:27:14 +0200

    keep trailing slash in abspath if given in input

--- Xhpc/test/test_loc.sh
+++ Xhpc/test/test_loc.sh
@@ -1,5 +1,5 @@
 # cp /Users/franck/programs/Xpbs/Xpbs/test/to_print_for_test.txt /Users/franck/programs/Xpbs/Xpbs/test/to_print_for_test_copy.txt | compare_genes.py -a qiime
-cp /Users/franck/programs/Xpbs/Xpbs/test/to_print_for_test.txt:/Users/franck/programs/Xpbs/Xpbs/test/to_print_for_test_copy.txt -o /Users/franck/programs/Xpbs/Xpbs/test -f /Users/franck/programs -g="a,/1/2/b,LICENCE" -x qiime -L LICENCE -c /Users/franck/.bash_sessions/4A708E48-6C2F-41A4-8BD0-27F12A2FF583.history
+cp /Users/franck/programs/Xpbs/Xpbs/test/to_print_for_test.txt:/Users/franck/programs/Xpbs/Xpbs/test/to_print_for_test_copy.txt -o /Users/franck/programs/Xpbs/Xpbs/test -f /Users/franck/programs -g="a,/1/2/b,LICENCE" -x qiime -L LICENCE -c /Users/franck/
 #cd file="qiime" -d "/file" -x "qiime file" -i /kjdb /kslj
 #file="qiime"
 #"quoted stuff" [ -f "$file"]
